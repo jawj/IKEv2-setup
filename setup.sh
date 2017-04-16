ac2fc6ec2e417bd8fddd89fd2cd6a1061ecf7c55
George MacKerron <george@mackerron.co.uk>
2017-04-16 19:35:45 +0200

    Turn off IPv6 autoconf until I know more about IPv6

--- setup.sh
+++ setup.sh
@@ -174,6 +174,7 @@ net.ipv4.ip_no_pmtu_disc = 1
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.all.send_redirects = 0
+net.ipv6.conf.all.autoconf = 0
 ' >> /etc/sysctl.conf
 
 sysctl -p
