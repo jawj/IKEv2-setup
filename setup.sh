5662c3dacb337f374cb2390f4d50743bd462dae1
George MacKerron <george@mackerron.co.uk>
2017-04-17 07:46:31 +0100

    Disable IPv6 instead

--- setup.sh
+++ setup.sh
@@ -174,7 +174,10 @@ net.ipv4.ip_no_pmtu_disc = 1
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.all.send_redirects = 0
-net.ipv6.conf.all.autoconf = 0
+
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
 ' >> /etc/sysctl.conf
 
 sysctl -p
